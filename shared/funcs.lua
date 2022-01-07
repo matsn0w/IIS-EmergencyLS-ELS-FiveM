7604396498cdcf61e8fa8ccce1cb34e0a79fb647
matsn0w <bartscholtus@gmail.com>
2022-01-07 14:36:53 +0100

    Add additional env lighting options Fixes matsn0w/IIS-EmergencyLS-ELS-FiveM#4

--- shared/funcs.lua
+++ shared/funcs.lua
@@ -8,31 +8,6 @@ function TableHasValue(table, value)
     return false
 end
 
-function CreateEnviromentLight(vehicle, extra, offset, color)
-    -- TODO: beam light from extra bone instead of the vehicle
-    -- local boneIndex = GetEntityBoneIndexByName(vehicle, 'extra_' .. extra)
-    -- local coords = GetWorldPositionOfEntityBone(vehicle, boneIndex)
-
-    -- get the coord of a position around the car
-    local position = GetOffsetFromEntityInWorldCoords(vehicle, offset.x, offset.y, offset.z)
-    local x, y, z = table.unpack(position)
-
-    local rgb = { 0, 0, 0 }
-    local range = 10.0
-    local intensity = 0.5
-    local shadow = 5.1
-
-    if string.lower(color) == 'blue' then rgb = { 0, 0, 255 }
-    elseif string.lower(color) == 'red' then rgb = { 255, 0, 0 }
-    elseif string.lower(color) == 'green' then rgb = { 0, 255, 0 }
-    elseif string.lower(color) == 'white' then rgb = { 255, 255, 255 }
-    elseif string.lower(color) == 'amber' then rgb = { 255, 194, 0}
-    end
-
-    -- draw the light
-    DrawLightWithRangeAndShadow(x, y, z, rgb[1], rgb[2], rgb[3], range, intensity, shadow)
-end
-
 function GetCarHash(car)
     if not car then return false end
 
