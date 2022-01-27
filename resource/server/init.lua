5f4df3a4a84f5d0702654de378b9815c63c59b34
matsn0w <bartscholtus@gmail.com>
2022-01-27 19:45:37 +0100

    feat: add update check
    Fixes #45

--- resource/server/init.lua
+++ resource/server/init.lua
@@ -1,9 +1,45 @@
 kjxmlData = {}
 
-function ParseObjSet(data, fileName)
+local function parseObjSet(data, fileName)
     local xml = SLAXML:dom(data)
 
     if xml and xml.root and xml.root.name == 'vcfroot' then ParseVCF(xml, fileName) end
+end
+
+local function checkForUpdate()
+    local versionFile = LoadResourceFile(GetCurrentResourceName(), 'version.json')
+
+    if not versionFile then
+        print('Couldn\'t read version file!')
+        return
+    end
+
+    local currentVersion = Semver(json.decode(versionFile)['version'] or 'unknown')
+
+    if currentVersion.prerelease then
+        print('WARNING: you are using a pre-release of MISS ELS!')
+        print('>> This version might be unstable and probably contains some bugs.')
+        print('>> Please report bugs and other problems via https://github.com/matsn0w/MISS-ELS/issues')
+    end
+
+    PerformHttpRequest('https://api.github.com/repos/matsn0w/MISS-ELS/releases/latest"', function(status, response, headers)
+        if status ~= 200 then
+            print('Something went wrong! Couldn\'t fetch latest version. Status: ' .. tostring(status))
+            return
+        end
+
+        local newVersion = Semver(json.decode(response)['tag_name'] or 'unknown')
+
+        if newVersion > currentVersion then
+            print('---------------------------- MISS ELS ----------------------------')
+            print('--------------------- NEW VERSION AVAILABLE! ---------------------')
+            print('>> You are using v' .. tostring(currentVersion))
+            print('>> You can upgrade to v' .. tostring(newVersion))
+            print()
+            print('Download it at https://github.com/matsn0w/MISS-ELS/releases/latest')
+            print('------------------------------------------------------------------')
+        end
+    end)
 end
 
 AddEventHandler('onResourceStart', function(name)
@@ -19,12 +55,16 @@ AddEventHandler('onResourceStart', function(name)
         return
     end
 
+    Citizen.CreateThread(function()
+        checkForUpdate()
+    end)
+
     for i = 1, #Config.ELSFiles do
         local file = Config.ELSFiles[i]
         local data = LoadResourceFile(GetCurrentResourceName(), 'xmlFiles/' .. file)
 
         if data then
-            if pcall(function() ParseObjSet(data, file) end) then
+            if pcall(function() parseObjSet(data, file) end) then
                 -- no errors
                 print('Parsed VCF for: ' .. file)
             else
