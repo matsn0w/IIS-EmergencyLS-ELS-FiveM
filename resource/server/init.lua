a3f4c967e2bbdf6197494bb2d26523fbb990f0dc
MisterDarkSideYT <79758423+MisterDarkSideYT@users.noreply.github.com>
2024-02-03 16:44:20 +0100

    Update init.lua

--- resource/server/init.lua
+++ resource/server/init.lua
@@ -1,92 +1,292 @@
---- The determined OS of the server
---- @type string|nil
-SystemOS = nil
+kjxmlData = {}
 
---- Check if there is a newer version of the resource available
+local systemOS = nil
+
+
+
+local function parseObjSet(data, fileName)
+
+    local xml = SLAXML:dom(data)
+
+
+
+    if xml and xml.root and xml.root.name == 'vcfroot' then ParseVCF(xml, fileName) end
+
+end
+
+
+
 local function checkForUpdate()
+
     local versionFile = LoadResourceFile(GetCurrentResourceName(), 'version.json')
 
+
+
     if not versionFile then
+
         print('Couldn\'t read version file!')
+
         return
+
     end
+
+
 
     local currentVersion = Semver(json.decode(versionFile)['version'] or 'unknown')
 
+
+
     if currentVersion.prerelease then
+
         print('WARNING: you are using a pre-release of MISS ELS!')
+
         print('>> This version might be unstable and probably contains some bugs.')
+
         print('>> Please report bugs and other problems via https://github.com/matsn0w/MISS-ELS/issues')
+
     end
 
+
+
     PerformHttpRequest('https://api.github.com/repos/matsn0w/MISS-ELS/releases/latest', function(status, response, headers)
+
         if status ~= 200 then
+
             print('Something went wrong! Couldn\'t fetch latest version. Status: ' .. tostring(status))
+
             return
+
         end
+
+
 
         local newVersion = Semver(json.decode(response)['tag_name'] or 'unknown')
 
+
+
         if newVersion > currentVersion then
+
             print('---------------------------- MISS ELS ----------------------------')
+
             print('--------------------- NEW VERSION AVAILABLE! ---------------------')
+
             print('>> You are using v' .. tostring(currentVersion))
+
             print('>> You can upgrade to v' .. tostring(newVersion))
+
             print()
+
             print('Download it at https://github.com/matsn0w/MISS-ELS/releases/latest')
+
             print('------------------------------------------------------------------')
+
         end
+
     end)
+
 end
 
-local function parseObjSet(data, fileName)
-    local xml = SLAXML:dom(data)
 
-    if xml and xml.root and xml.root.name == 'vcfroot' then ParseVCF(xml, fileName) end
+
+local function determineOS()
+
+    local system = nil
+
+
+
+    local unix = os.getenv('HOME')
+
+    local windows = os.getenv('HOMEPATH')
+
+
+
+    if unix then system = 'unix' end
+
+    if windows then system = 'windows' end
+
+
+
+    -- this guy probably has some custom ENV var set...
+
+    if unix and windows then error('Couldn\'t identify the OS unambiguously.') end
+
+
+
+    return system
+
 end
 
-local function loadVcfs(folder)
-    for _, file in pairs(ScanDirectory(folder)) do
-        local data = LoadFile(folder .. '/' .. file)
 
-        if data then
-            if pcall(function() parseObjSet(data, file) end) then
-                --- no errors
-                Print('Parsed VCF for: ' .. file, PrintColors.GREEN)
-            else
-                --- VCF is faulty, notify the user and continue
-                Print('VCF file ' .. file .. ' could not be parsed: is your XML valid?', PrintColors.YELLOW)
-            end
-        else
-            Print('VCF file ' .. file .. ' not found: does the file exist?', PrintColors.YELLOW)
-        end
+
+local function scanDir(folder)
+
+    local pathSeparator = '/'
+
+    local command = 'ls -A'
+
+
+
+    if systemOS == 'windows' then
+
+        pathSeparator = '\\'
+
+        command = 'dir /R /B'
+
     end
+
+
+
+    local resourcePath = GetResourcePath(GetCurrentResourceName())
+
+    local directory = resourcePath .. pathSeparator .. folder
+
+    local i, t, popen = 0, {}, io.popen
+
+    local pfile = popen(command .. ' "' .. directory .. '"')
+
+
+
+    for filename in pfile:lines() do
+
+        i = i + 1
+
+        t[i] = filename
+
+    end
+
+
+
+    if #t == 0 then
+
+        error('Couldn\'t find any VCF files. Are they in the correct directory?')
+
+    end
+
+
+
+    pfile:close()
+
+    return t
+
 end
+
+
+
+local function loadFile(file)
+
+    return LoadResourceFile(GetCurrentResourceName(), file)
+
+end
+
+
 
 AddEventHandler('onResourceStart', function(name)
+
     if not Config then
+
         error('You probably forgot to copy the example configuration file. Please see the installation instructions for further details.')
+
         StopResource(GetCurrentResourceName())
+
         CancelEvent()
+
         return
+
     end
+
+
 
     if name:lower() ~= GetCurrentResourceName():lower() then
+
         CancelEvent()
+
         return
+
     end
 
+
+
     Citizen.CreateThread(function()
+
         checkForUpdate()
+
     end)
 
-    SystemOS = DetermineServerOS()
 
-    if not SystemOS then
+
+    local folder = 'xmlFiles'
+
+
+
+    -- determine the server OS
+
+    systemOS = determineOS()
+
+
+
+    if not systemOS then
+
         error('Couldn\'t determine your OS! Are your running on steroids??')
+
     end
 
-    Citizen.CreateThread(function()
-        loadVcfs('xmlFiles')
-    end)
+
+
+    for _, file in pairs(scanDir(folder)) do
+
+        local data = loadFile(folder .. '/' .. file)
+
+
+
+        if data then
+
+            if pcall(function() parseObjSet(data, file) end) then
+
+                -- no errors
+
+                print('Parsed VCF for: ' .. file)
+
+            else
+
+                -- VCF is faulty, notify the user and continue
+
+                print('VCF file ' .. file .. ' could not be parsed: is your XML valid?')
+
+            end
+
+        else
+
+            print('VCF file ' .. file .. ' not found: does the file exist?')
+
+        end
+
+    end
+
+
+
+    -- send the ELS data to all clients
+
+    TriggerClientEvent('kjELS:sendELSInformation', -1, kjxmlData)
+
 end)
+
+
+
+RegisterServerEvent('kjELS:requestELSInformation')
+
+AddEventHandler('kjELS:requestELSInformation', function()
+
+    TriggerClientEvent('kjELS:sendELSInformation', source, kjxmlData)
+
+end)
+
+
+
+RegisterNetEvent('baseevents:enteredVehicle')
+
+AddEventHandler('baseevents:enteredVehicle', function(veh, seat, name)
+
+    TriggerClientEvent('kjELS:initVehicle', source)
+
+end)
+
