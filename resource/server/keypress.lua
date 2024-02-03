2c43968043db4fad5a17c4c08617aaa5dcf2babb
MisterDarkSideYT <79758423+MisterDarkSideYT@users.noreply.github.com>
2024-02-03 16:44:55 +0100

    Update keypress.lua

--- resource/server/keypress.lua
+++ resource/server/keypress.lua
@@ -1,4 +1,28 @@
-RegisterServerEvent('MISS-ELS:server:toggleHorn')
-AddEventHandler('MISS-ELS:server:toggleHorn', function(state)
-    TriggerClientEvent('MISS-ELS:client:updateHorn', -1, source, state)
+RegisterServerEvent('kjELS:setSirenState')
+
+AddEventHandler('kjELS:setSirenState', function(state)
+
+    TriggerClientEvent('kjELS:updateSiren', -1, source, state)
+
 end)
+
+
+
+RegisterServerEvent('kjELS:toggleHorn')
+
+AddEventHandler('kjELS:toggleHorn', function(state)
+
+    TriggerClientEvent('kjELS:updateHorn', -1, source, state)
+
+end)
+
+
+
+RegisterNetEvent('kjELS:sv_Indicator')
+
+AddEventHandler('kjELS:sv_Indicator', function(direction, toggle)
+
+    TriggerClientEvent('kjELS:updateIndicators', source, direction, toggle)
+
+end)
+
